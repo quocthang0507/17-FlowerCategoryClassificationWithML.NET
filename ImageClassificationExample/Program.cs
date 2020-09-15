4a8e032a1b45d8c899342834fddaf5c95ecaf8bc
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2020-09-15 09:34:06 +0700

    Update file

--- ImageClassificationExample/Program.cs
+++ ImageClassificationExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ImageClassificationExampleML.Model;
 using System;
+using System.Linq;
 
 namespace ImageClassificationExample
 {
@@ -9,7 +10,6 @@ namespace ImageClassificationExample
 		{
 			while (true)
 			{
-				Console.Clear();
 				Console.WriteLine("Here are some categories that the model can recognize");
 				foreach (var cat in Enum.GetValues(typeof(Category)))
 				{
@@ -19,6 +19,10 @@ namespace ImageClassificationExample
 
 				Console.Write("\nPaste the image path which want to recognize: ");
 				string source = Console.ReadLine();
+				if (source == "exit")
+				{
+					break;
+				}
 
 				ModelInput sampleData = new ModelInput()
 				{
@@ -28,7 +32,8 @@ namespace ImageClassificationExample
 				var predictionResult = ConsumeModel.Predict(sampleData);
 
 				Console.WriteLine($"ImageSource: {sampleData.ImageSource}");
-				Console.WriteLine($"\n\nPredicted Label value {predictionResult.Prediction}\n");
+				Console.WriteLine($"\n\nPredicted Label value {predictionResult.Prediction}");
+				Console.WriteLine($"\nAccuracy: {predictionResult.Score.Max()}\n");
 			}
 			Console.WriteLine("=============== End of process, hit any key to finish ===============");
 			Console.ReadKey();
